4bfa3a8bfc515fe05dbe951f7778b36bbb8ee0a9
amirbapiri <amirbapiri@gmail.com>
2021-07-25 03:22:22 +0430

    Determine end points in root urls

--- sms_registration/urls.py
+++ sms_registration/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from accounts import urls as account_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # accounts
+    path("accounts/", include((account_urls, "accounts"), namespace="accounts")),
 ]
