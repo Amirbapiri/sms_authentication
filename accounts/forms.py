a2c091e7eb123605889a97c51490ae2eda50c526
amirbapiri <amirbapiri@gmail.com>
2021-07-25 12:41:35 +0430

    Create needed forms for users authentication

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,16 @@
 from django import forms
+from django.contrib.auth import get_user_model
+
+
+class RegistrationForm(forms.ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = ("mobile",)
 
 
 class LoginForm(forms.Form):
     mobile = forms.CharField(max_length=11, required=True)
+
+
+class OTPVerificationForm(forms.Form):
+    otp = forms.CharField(max_length=5, required=True)
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 
-from accounts.views import panel, UserLoginView
+from accounts.views import (
+    panel,
+    UserLoginView,
+    UserRegistrationView,
+    verify_otp
+)
 
 urlpatterns = [
+    path("signup/", UserRegistrationView.as_view(), name="signup"),
     path("login/", UserLoginView.as_view(), name="login"),
+    path("verification/", verify_otp, name="otp_verification"),
     path("panel/", panel, name="panel")
 ]
